4f76acf2410c05e1d46f8ee68222ffb242976c4d
Lars <lars.timmerman@student.hu.nl>
2015-02-10 15:24:18 +0100

    Initial commit

---
+++ src/practicum2_1/DamspelApp.java
@@ -0,0 +1,90 @@
+package practicum2_1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class DamspelApp extends Application implements
+		EventHandler<ActionEvent> {
+	private Button[][] buttonbord = new Button[10][10];
+	private Button reset = new Button("Reset");
+	private Label melding = new Label("Meldingen komen hier!");
+	private CheckBox roteren = new CheckBox();
+	private Label speler = new Label("Speler :");
+	private Label stenenZ = new Label("Stenen Z: ");
+	private Label stenenW = new Label("Stenen W: ");
+	private Damspel spel = new Damspel();
+
+	@Override
+	public void start(Stage primaryStage) {
+		BorderPane root = new BorderPane();
+		GridPane bord = new GridPane();
+		VBox links = new VBox();
+		links.getChildren().addAll(reset, roteren, speler, stenenZ, stenenW);
+		links.setSpacing(3);
+		links.setPadding(new Insets(20, 0, 0, 10));
+
+		speler.setPrefSize(100, 30);
+		stenenZ.setPrefSize(100, 30);
+		stenenW.setPrefSize(100, 30);
+
+		bord.setPrefSize(300, 200);
+		bord.setAlignment(Pos.BOTTOM_RIGHT);
+		
+		
+		reset.setOnAction(this);
+		reset.setPrefSize(120, 20);
+		reset.getStyleClass().add("reset");
+
+		melding.getStyleClass().add("melding");
+		root.setAlignment(melding, Pos.TOP_RIGHT);
+		root.setPadding(new Insets(5, 5, 5, 5));
+		root.setTop(melding);
+
+		int k = 0;
+		for (int i = 0; i < buttonbord.length; i++) {
+			for (int j = 0; j < buttonbord.length; j++) {
+				buttonbord[i][j] = new Button();
+				buttonbord[i][j].setOnAction(this);
+				buttonbord[i][j].setPrefSize(40, 40);
+				buttonbord[i][j].setId(String.valueOf(k));
+				buttonbord[i][j].getStyleClass().add(spel.getVeldStatus(k));
+				k++;
+
+				bord.getChildren().add(buttonbord[i][j]);
+				bord.setRowIndex(buttonbord[i][j], i);
+				bord.setColumnIndex(buttonbord[i][j], j);
+
+			}
+		}
+
+		Scene scene = new Scene(root, 550, 430);
+		scene.getStylesheets().add("practicum2_1/stylesheet.css");
+		root.getStylesheets().add("practicum2_1/stylesheet.css");
+		root.setCenter(bord);
+		root.setLeft(links);
+		primaryStage.setTitle("DamApp");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setResizable(false);
+	}
+
+	public void handle(ActionEvent event) {
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+}
